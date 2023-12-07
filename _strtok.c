a19fc30448e149288640f18c92a8611465c9fe62
Munjogu123 <fredmunjogu@gmail.com>
2023-12-07 00:40:37 -0800

    test

---
+++ _strtok.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char **split_string(char *str, int num)
+{
+	char **arr, *dup, *piece;
+	int i;
+
+	if (str == NULL)
+		return (NULL);
+
+	dup = strdup(str);
+	piece = strtok(dup, " ");
+	while (piece != NULL)
+	{
+		if (*piece == '\n')
+		{
+			piece = strtok(NULL, " ");
+			continue;
+		}
+		num++;
+		piece = strtok(NULL, " ");
+	}
+
+	arr = malloc(sizeof(char *) * num);
+	piece = strtok(str, " ");
+	i = 0;
+	while (piece != NULL)
+	{
+		if (*piece == '\n')
+		{
+			piece = strtok(NULL, " ");
+			continue;
+		}
+		arr[i] = malloc(sizeof(char) * (strlen(piece) + 1));
+		strcpy(arr[i], piece);
+		piece = strtok(NULL, " ");
+	}
+
+	if (dup)
+		free(dup);
+
+	return (arr);
+}
+
+int main(void)
+{
+	char *str = "Test string";
+	int i, num = 0;
+	char **result = split_string(str, num);
+
+	for (i = 0; i < num; i++)
+	{
+		printf("word[%d]: %s\n", i, result[i]);
+		free(result[i]);
+	}
+	free(result);
+
+	return (0);
+}
